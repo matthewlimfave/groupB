4539ec3f201cf706287b01cbee1a8ec22b12e098
Matt Fave <matthew.lim@myfave.com>
2022-08-29 11:57:43 +0800

    init

---
+++ src/test/java/com/pinelabs/groupB/GroupBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinelabs.groupB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
